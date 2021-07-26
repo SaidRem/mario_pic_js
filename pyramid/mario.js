a05620d96c0a553f466eb590f344c0f82da8c14e
SaidRem <said.r@yandex.ru>
2021-07-26 22:25:27 +0400

    Added draw_pyramid_2() version 2

--- pyramid/mario.js
+++ pyramid/mario.js
@@ -7,3 +7,20 @@ function draw_pyramid(height) {
         pyramid_scope.appendChild(div_node);
     }
 }
+
+// Creating Pyramid version 2
+function draw_pyramid_2(height) {
+    for (let i=2; i <= height; i++) {
+        let div_parent = document.createElement('div');
+        for (let j=0; j <= height-1; j++) {
+            let white_squares = height - i;
+            let div_child = document.createElement('div');
+            div_child.setAttribute('class', 'square');
+            if (j < white_squares) div_child.style.background = "white"
+            div_parent.appendChild(div_child);
+            document.body.append(div_parent);
+        }
+    }
+}
+
+
