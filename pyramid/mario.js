198db63f41e8d319403132ca609368a31787934e
SaidRem <said.r@yandex.ru>
2021-07-26 22:26:40 +0400

    Added draw_pyramid_3() version 3

--- pyramid/mario.js
+++ pyramid/mario.js
@@ -23,4 +23,22 @@ function draw_pyramid_2(height) {
     }
 }
 
+// Creating Pyramid version 3
+function draw_pyramid_3(height) {
+    let pyramid_scope = document.getElementById('pyramid');
+    for (let i = 1; i < height; i++) {
+        div_parent = document.createElement('div');
+        for (let j1 = 0; j1 < (height-i); j1++) {
+            div_space = document.createElement('div');
+            div_space.setAttribute('class', 'space');
+            div_parent.appendChild(div_space);
+        }
+        for (let j2 = 0; j2 < (i+1); j2++){
+            div_square = document.createElement('div');
+            div_square.setAttribute('class', 'square');
+            div_parent.appendChild(div_square);
+        }
+        pyramid_scope.appendChild(div_parent)
+    }
+}
 
